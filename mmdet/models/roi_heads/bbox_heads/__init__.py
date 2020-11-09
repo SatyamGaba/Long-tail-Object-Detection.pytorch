7da8e288958263611ce670d57ab1a8495b8d9665
OJessicaO <opjessica5@gmail.com>
2020-11-09 03:27:43 +0000

    added lvis_analyze and bbox_head

--- mmdet/models/roi_heads/bbox_heads/__init__.py
+++ mmdet/models/roi_heads/bbox_heads/__init__.py
@@ -3,8 +3,10 @@ from .convfc_bbox_head import (ConvFCBBoxHead, Shared2FCBBoxHead,
                                Shared4Conv1FCBBoxHead)
 from .double_bbox_head import DoubleConvFCBBoxHead
 from .sabl_head import SABLHead
+from .gs_bbox_head_with0 import GSBBoxHeadWith0
 
 __all__ = [
     'BBoxHead', 'ConvFCBBoxHead', 'Shared2FCBBoxHead',
-    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'SABLHead'
+    'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'SABLHead', 
+    'GSBBoxHeadWith0'
 ]
