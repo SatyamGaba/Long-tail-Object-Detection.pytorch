8eda648acaa3c0cc775bace6e0b71b23411bbc8a
SatyamGaba <satyamgb321@gmail.com>
2020-11-24 04:54:26 +0000

    uncommitted changes

--- lvis/eval.py
+++ lvis/eval.py
@@ -11,14 +11,6 @@ from .results import LVISResults
 
 import sys
 
-def sizeof_fmt(num, suffix='B'):
-    ''' by Fred Cirera,  https://stackoverflow.com/a/1094933/1870254, modified'''
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
-        if abs(num) < 1024.0:
-            return "%3.1f %s%s" % (num, unit, suffix)
-        num /= 1024.0
-    return "%.1f %s%s" % (num, 'Yi', suffix)
-
 class LVISEval:
     def __init__(self, lvis_gt, lvis_dt, iou_type='segm'):
         """Constructor for LVISEval.
